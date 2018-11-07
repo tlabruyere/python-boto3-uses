77f6b8150b0768e07a63db98921c440975ec09f2
Tom LaBruyere <tom.labruyere@gmail.com>
2018-11-06 23:09:46 -0700

    cleaning up comments

--- cleanupAMI.py
+++ cleanupAMI.py
@@ -7,6 +7,12 @@ def load_env(var_file):
         env = json.load(f)
     return env
 
+
+def sort_images_by_creation_date(images):
+    newlist = sorted(images['Images'], key=lambda k: k['CreationDate'],reverse=True) 
+    print(newlist[0])
+
+
 class AWS_Interaction(object):
     '''
     Handles interactions for AWS
@@ -89,36 +95,6 @@ class AWS_Interaction(object):
 
     def get_amis_by_owner(self, owner_id, key=None):
         images = self.describe_images(Owners=owner_id)
-#        images =  {'Images': [{'Architecture': 'x86_64',
-#                'CreationDate': '2018-07-24T03:39:09.000Z',
-#                'ImageId': 'ami-01628f22f4c4941c1',
-#                'ImageLocation': '823307771695/packer-example 1532403465',
-#                'ImageType': 'machine',
-#                'Public': False,
-#                'OwnerId': '823307771695',
-#                'State': 'available',
-#                'BlockDeviceMappings': [{'DeviceName': '/dev/sda1',
-#                'Ebs': {'Encrypted': False,
-#                'DeleteOnTermination': True,
-#                'SnapshotId': 'snap-011584176c08aad96',
-#                'VolumeSize': 8,
-#                'VolumeType': 'gp2'}},
-#                {'DeviceName': '/dev/sdb', 'VirtualName': 'ephemeral0'},
-#                {'DeviceName': '/dev/sdc', 'VirtualName': 'ephemeral1'}],
-#                'EnaSupport': True,
-#                'Hypervisor': 'xen',
-#                'Name': 'packer-example 1532403465',
-#                'RootDeviceName': '/dev/sda1',
-#                'RootDeviceType': 'ebs',
-#                'SriovNetSupport': 'simple',
-#                'VirtualizationType': 'hvm'}],
-#                'ResponseMetadata': {'RequestId': '3eadc2d9-2d3c-4661-830e-b7cb2f82be56',
-#                'HTTPStatusCode': 200,
-#                'HTTPHeaders': {'content-type': 'text/xml;charset=UTF-8',
-#                'content-length': '1888',
-#                'date': 'Tue, 24 Jul 2018 03:41:01 GMT',
-#                'server': 'AmazonEC2'},
-#                'RetryAttempts': 0}}
         if key is None:
             return images['Images']
         if 0 < len(images['Images'][0]) and key not in images['Images'][0].keys():
@@ -128,10 +104,3 @@ class AWS_Interaction(object):
     def deregister_ami(self, li_o_ami_id):
         for ami in li_o_ami_id:
             self.client().deregister_image(ImageId=ami)
-    
-#    connection = boto3.ec2.connect_to_region('eu-west-1', 
-#            aws_access_key_id=env['aws_access_key'], \
-#            aws_secret_access_key=env['aws_secret_key'])
-#            proxy=yourProxy, \
-#            proxy_port=yourProxyPort)
-#    return connection.get_all_images()
